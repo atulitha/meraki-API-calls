0271bab805f6750be4d428d8d73b980c9e598b59
atulitha revanth <pandu.revanth@gmail.com>
2021-02-20 16:38:41 +0530

    initial commit

--- api call to excel.py
+++ api call to excel.py
@@ -1,5 +1,4 @@
 import json
-
 import pandas as pd
 import requests
 
@@ -8,33 +7,42 @@ if __name__ == "__main__":
         lines = [line.strip() for line in n]
         networks = lines
     device = networks
-    print(networks)
     outlist = []
+    outlist2 = []
+
     for network in networks:
         print(network)
         try:
             payload = {}
-            url = "https://api-mp.meraki.com/api/v1/networks/" + network + "/devices"
+            url = "https://api-mp.meraki.com/api/v1/networks/" + network + "/clients?perPage=999"
             headers = {
-                'X-Cisco-Meraki-API-Key': 'Your api key here'
+                'X-Cisco-Meraki-API-Key': 'Your API key here'
             }
             response = requests.request("GET", url, headers=headers, data=payload)
             x = json.loads(response.text)
 
             if isinstance(x, dict):
+                x.update({'Gi ven_id': network})
                 outlist.append(x)
+                print(pd.json_normalize(x))
             else:
-                outlist += x
+                for i in x:
+                    i.update({'Given_id': network})
+                    outlist2.append(i)
+
+
+                outlist += outlist2
         except Exception as e:
             print(e, "\ndevice is :", network)
             with open("errordevices.txt", "w") as ed:
                 ed.writelines(["this is failed: \n", network, "\n", "-" * 10])
-                outlist.append({"Error": 'this has failed'})
+                outlist.append({'Given_id': network, "Error": 'this has failed'})
+
 
     print(outlist)
-    print(networks)
 
-    df = pd.json_normalize(
-        [i for i in outlist])  # .to_excel('output.xlsx', index="networkId", header=True, engine='openpyxl')
-    df.insert(0, "Device", device, True)
-    df.to_excel('output.xlsx', index=False, header=True, engine='openpyxl')
+    df = pd.json_normalize([i for i in outlist])
+    df.set_index('Given_id')
+    df.to_excel('output.xlsx', index= True, header=True, engine='openpyxl',)
+
+
